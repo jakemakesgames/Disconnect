9acf35f5c2ca97e409387798bc27f7ff4c8b2bb4
jakemakesgames <jakebrown.designer@gmail.com>
2018-05-19 18:00:40 +1000

    added new interactable and consumable tag and layer. new item prperties and raycast manager scripts. player can now interact with food, water and health packs and they will add an amount to their survivor traits

---
+++ Disconnect/Assets/MyAssets/Scripts/Managers/ItemProperties.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemProperties : MonoBehaviour 
+{
+	[Header("Consumables")]
+	public string itemName;
+
+	[SerializeField] private bool food;
+	[SerializeField] private bool water;
+	[SerializeField] private bool health;
+	[SerializeField] private float value;
+
+	[SerializeField] SurvivorTraits survivorTraits;
+
+	public void Interaction()
+	{
+		//if the FOOD bool is true
+		if (food) 
+		{
+			survivorTraits.hungerSlider.value += value;
+		} 
+		// if the WATER bool is true
+		else if (water) 
+		{
+			survivorTraits.thirstSlider.value += value;
+		}
+		// if the HEALTH bool is true
+		else if (health) 
+		{
+			survivorTraits.healthSlider.value += value;
+		}
+	}
+
+}
---
+++ Disconnect/Assets/MyAssets/Scripts/Managers/RaycastManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RaycastManager : MonoBehaviour 
+{
+	private GameObject raycastedObj;
+
+	[Header("Raycast Settings")]
+	[SerializeField] private float rayLength = 10;
+	[SerializeField] private LayerMask newLayerMask;
+
+	[Header("References")]
+	[SerializeField] private Image reticle;
+	[SerializeField] private Text itemNameText;
+
+	void Update()
+	{
+		// Cast a raycast out forward from the player
+		RaycastHit hit;
+		Vector3 fwd = transform.TransformDirection (Vector3.forward);
+
+		// Check what the raycast hits
+		if (Physics.Raycast (transform.position, fwd, out hit, rayLength, newLayerMask.value)) {
+			if (hit.collider.CompareTag ("Consumable")) {
+				ReticleActive ();
+				raycastedObj = hit.collider.gameObject;
+
+				// Update the UI name
+				itemNameText.text = raycastedObj.GetComponent<ItemProperties>().itemName;
+
+				if (Input.GetMouseButtonDown (0)) 
+				{
+					// Select OBJ properties
+					raycastedObj.GetComponent<ItemProperties>().Interaction();
+					Destroy (raycastedObj);
+				}
+			}
+		} 
+		else 
+		{
+			// Update the UI name
+			ReticleNormal ();
+			itemNameText.text = null;
+		}
+
+	}
+
+	void ReticleActive()
+	{
+		// Sets the reticle colour to Green
+		reticle.color = Color.green;
+	}
+
+	void ReticleNormal()
+	{
+		// Sets the reticle crosshair to White
+		reticle.color = Color.white;
+	}
+
+}
