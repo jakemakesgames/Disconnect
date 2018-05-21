13229353a7f7998f0b22b694c0ff2a5289ea32b5
jakemakesgames <jakebrown.designer@gmail.com>
2018-05-21 22:21:04 +1000

    it works on my machine

---
+++ Disconnect/Assets/MyAssets/Scripts/Managers/TestManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestManager : MonoBehaviour 
+{
+	public DisableManager dm;
+
+	void Awake()
+	{
+		dm = GameObject.FindGameObjectWithTag ("DisableController").GetComponent<DisableManager> ();
+
+		dm.EnablePlayer ();
+	}
+
+
+
+}
