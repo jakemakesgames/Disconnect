e27d838bf24ce1bf5238f7e20963ab94205ac674
JordanDines <jordan@dines.net.au>
2018-06-01 00:34:59 +1000

    Created Endless_Halls scene. Set up two portals. Need to fix glitching between

---
+++ Disconnect/Assets/MyAssets/Scripts/PortalTeleporter.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleporter : MonoBehaviour {
+
+    public Transform player;
+    public Transform receiver;
+    public Transform portal;
+    private int angleToRotatePortal = 0;
+
+    public bool playerIsOverlapping = false;
+
+    void Start()
+    {
+        angleToRotatePortal = (int)portal.rotation.y;
+    }
+
+    void Update () {
+        if (playerIsOverlapping)
+        {
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            //If this is true: The player has moved across the portal
+            if(dotProduct < 0f)
+            {
+                //Teleport me
+                float rotationDiff = -Quaternion.Angle(transform.rotation, receiver.rotation);
+                rotationDiff += angleToRotatePortal;
+                player.Rotate(Vector3.up, rotationDiff);
+
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+                player.position = receiver.position + positionOffset;
+
+                playerIsOverlapping = false;
+            }
+        }
+	}
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            playerIsOverlapping = true;
+        }
+    }
+
+   private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = false;
+        }
+    }
+}
