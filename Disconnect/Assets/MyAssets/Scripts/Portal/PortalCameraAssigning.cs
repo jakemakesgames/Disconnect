0c9cd05ec1a1254be33dcef04d5ea430401e177c
Jordan Dines <jordan@dines.net.au>
2018-06-05 16:33:50 +1000

    Working on Third correct corridor. Currently broken, but functional.

---
+++ Disconnect/Assets/MyAssets/Scripts/Portal/PortalCameraAssigning.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCameraAssigning : MonoBehaviour {
+
+    [SerializeField] private GameObject correctCamera;
+
+    private PortalCameraTracking pct;
+	// Use this for initialization
+	void Start () {
+        pct = correctCamera.GetComponent<PortalCameraTracking>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        pct.secondRoom = true;
+    }
+
+}
--- Disconnect/Assets/MyAssets/Scripts/Portal/PortalCameraTracking.cs
+++ Disconnect/Assets/MyAssets/Scripts/Portal/PortalCameraTracking.cs
@@ -7,25 +7,52 @@ public class PortalCameraTracking : MonoBehaviour {
     private Transform playerCamera;
     [Tooltip("Reference the Receiver Portal Parent")]
     [SerializeField]
-    private Transform receiverPortal;
+    private Transform firstReceiverPortal;
+    [SerializeField]
+    private Transform secondReceiverPortal;
     [Tooltip("Reference the Sender Portal Parent with the corresponding letter suffix")]
     [SerializeField]
-    private Transform senderPortalToTrack;
+    private Transform firstSenderPortalToTrack;
+    [Tooltip("Reference the Sender Portal Parent with the corresponding letter suffix")]
+    [SerializeField]
+    private Transform secondSenderPortalToTrack;
+
+    public bool secondRoom = false;
 
     void Start()
     {
         playerCamera = GameObject.FindWithTag("Player").GetComponentInChildren<Camera>().transform;
+        if(secondSenderPortalToTrack == null && secondReceiverPortal == null)
+        {
+            //secondSenderPortalToTrack = null;
+            //secondReceiverPortal = null;
+            //return;
+        }
     }
 
     void Update()
     {
-        Vector3 playerOffsetFromPortal = playerCamera.position - senderPortalToTrack.position;
-        transform.position = receiverPortal.position + playerOffsetFromPortal;
+        if (!secondRoom)
+        {
+            Vector3 playerOffsetFromPortal = playerCamera.position - firstSenderPortalToTrack.position;
+            transform.position = firstReceiverPortal.position + playerOffsetFromPortal;
 
-        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(receiverPortal.rotation, senderPortalToTrack.rotation);
+            float angularDifferenceBetweenPortalRotations = Quaternion.Angle(firstReceiverPortal.rotation, firstSenderPortalToTrack.rotation);
 
-        Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
-        Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
-        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+            Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+            Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
+            transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+        }
+        else if (secondRoom)
+        {
+            Vector3 playerOffsetFromPortal = playerCamera.position - secondSenderPortalToTrack.position;
+            transform.position = secondReceiverPortal.position + playerOffsetFromPortal;
+
+            float angularDifferenceBetweenPortalRotations = Quaternion.Angle(secondReceiverPortal.rotation, secondSenderPortalToTrack.rotation);
+
+            Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+            Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
+            transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+        }
     }
 }
