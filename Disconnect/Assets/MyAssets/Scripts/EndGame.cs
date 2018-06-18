acd88e6112b3103e56d91b8e867ee61d0955aeb7
jakemakesgames <jakebrown.designer@gmail.com>
2018-06-19 00:36:43 +1000

    end game scripte.. is fucked

---
+++ Disconnect/Assets/MyAssets/Scripts/EndGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class EndGame : MonoBehaviour 
+{
+	public GameObject platform;
+	public float speed;
+	public Transform endPoint;
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.CompareTag ("Player")) 
+		{
+			platform.GetComponent<Rigidbody> ();
+
+			other.GetComponent<FirstPersonController> ().enabled = false;
+
+			float moveSpd = speed * Time.deltaTime;
+			platform.transform.position = Vector3.MoveTowards (platform.transform.position, endPoint.position, moveSpd);
+		}
+	}
+}
