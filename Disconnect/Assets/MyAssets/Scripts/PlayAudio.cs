e99f57bb3812e4db5582206698fc9aeb120ba5aa
JordanDines <jordan@dines.net.au>
2018-06-18 22:40:17 +1000

    Added custom audio trigger.
    Added correct portal sound

---
+++ Disconnect/Assets/MyAssets/Scripts/PlayAudio.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour {
+
+    [SerializeField] private AudioSource audioClip;
+    [SerializeField] private bool isPlaying;
+    [SerializeField] private bool hasPlayed;
+
+
+    private void Update()
+    {
+       if(audioClip.isPlaying == true)
+        {
+            hasPlayed = true;
+            isPlaying = true;
+        }
+        if (hasPlayed && !isPlaying)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !isPlaying)
+        {
+            audioClip.Play();
+        }
+    }
+
+}
